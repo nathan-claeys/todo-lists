c8b60c6905d8087ff1e78c2f3af90c67d0d284ae
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2024-11-19 17:56:05 +0100

    save front part of the app

--- src/plugins/db.ts
+++
@@ -1,6 +0,0 @@
-import fp from 'fastify-plugin'
-import level, { FastifyLeveldbOptions } from '@fastify/leveldb'
-
-export default fp<FastifyLeveldbOptions>(async (fastify) => {
-  fastify.register(level, {name: 'db', path: './db'})
-})
